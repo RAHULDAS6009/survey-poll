1e86f689d1ec226b62880ceac68a4fafd9dd8a43
RAHULDAS6009 <das206053@gmail.com>
2024-10-11 12:39:24 +0530

    server init code and schema made

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-const a: number = 10;
+import express, { Express, Request, Response } from "express";
 
-console.log(a);
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("hello from server")
+});
+app.listen(5000, () => {
+  console.log("sever is running ");
+});
---
+++ src/routes/surveyRoute.ts
@@ -0,0 +1,5 @@
+// - `GET /surveys`: Fetch all surveys.
+// - `POST /surveys`: Create a new survey.
+// - `GET /surveys/:id`: Fetch a specific survey.
+// - `PUT /surveys/:id`: Update a specific survey.
+// - `DELETE /surveys/:id`: Delete a specific survey.
